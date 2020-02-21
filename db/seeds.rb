f8472479708924610904aaa0c5fb23dae7d64e5a
guillaume veldekens <guillaume.veldekens@gmail.com>
2020-02-21 16:54:44 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,3 +10,11 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ingredients = JSON.parse(open(url).read)
+xx = ingredients["drinks"]
+xx.each do |x|
+  Ingredient.create(name: x['strIngredient1'])
+end
